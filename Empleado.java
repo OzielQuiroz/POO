7ff4f83ae9d9d1f99506b75b9959528382f5d3c1
OzielQuiroz <39919727+OzielQuiroz@users.noreply.github.com>
2025-03-16 19:18:48 -0600

    Update Empleado.java

--- Empleado.java
+++ Empleado.java
@@ -1,18 +1,18 @@
 
 package com.mycompany.poo;
 
-public class Empleado {
-    private String nombre;
+public class Empleado {//Se define la clase
+    private String nombre;//EL nombre de la clase
     private int id;
     private double salario;
 
-    public Empleado(String nombre, int id, double salario) {
+    public Empleado(String nombre, int id, double salario) { 
         this.nombre = nombre;
         this.id = id;
         this.salario = salario;
     }
 
-    public String getNombre() {
+    public String getNombre() {//Se le cean los atributos
         return nombre;
     }
 
@@ -23,11 +23,11 @@ public class Empleado {
     public double getSalario() {
         return salario;
     }
-
-    public void aumentarSalario(double porcentaje) {
+//Se crea un metodo para aumentar el atributo que e aumentara en un futuro
+    public void aumentarSalario(double porcentaje) {//
         this.salario += this.salario * (porcentaje / 100);
     }
-
+//Con override podremos llamar y modificar el objeto llamado Empleado para su futura modificacion
     @Override
     public String toString() {
         return "Empleado [nombre=" + nombre + ", id=" + id + ", salario=" + salario + "]";
